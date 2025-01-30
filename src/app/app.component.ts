2c1e80de72ab7cd02e93bd72064b4dfb292e7ed7
Ouattara Kiboyou mohamed <86033140+kiboyou@users.noreply.github.com>
2025-01-30 22:38:51 +0100

    push 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { TaskHeaderComponent } from './components/task-header/task-header.component';
@@ -7,10 +6,12 @@ import { TaskListComponent } from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TaskListComponent, TaskHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'gestTask';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    initFlowbite();
+  }
 }
